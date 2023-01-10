d2c4234b5564e849141db3724afaab18eeff3913
highdroo <113620035+highdroo@users.noreply.github.com>
2023-01-10 14:39:35 +0400

    2 seite fast fertig und java script angefangen POG

---
+++ readme.md
@@ -0,0 +1,10 @@
+https://chat.openai.com/chat/763e1520-7b23-4531-a9aa-f2da80ca9edb
+https://github.com/animate-css/animate.css/blob/main/animate.css
+https://www.awwwards.com/inspiration_search/?text=b%C3%A4ume
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/find-the-length-of-a-string
+https://www.figma.com/file/27udIqZ4zLKsHXvIar2kFp/Untitled?node-id=0%3A3&t=ebSJ3eitfCRa30PH-0
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,67 @@
+const answerButton = document.getElementById("answer-button");
+const answerElement = document.getElementById("answer");
+var rightanswers = 0;
+answerElement.style.color ="red";
+
+answerButton.addEventListener("click", function () {
+    answerElement.innerHTML = answer;
+    
+    const q1 = document.getElementsByName("q1");
+    console.log(q1);
+    q1.forEach((e) => {
+        console.log(e);
+        //e.parentElement.style.backgroundColor = (e.value == "true")? 'green':'red';
+        e.parentElement.style.backgroundColor = 'transparent';
+
+        if (e.checked == true) {
+            if (e.value == "true") {
+                e.parentElement.style.backgroundColor = 'green';
+                rightanswers++;
+            }
+            else {
+                e.parentElement.style.backgroundColor = 'red';
+            }
+        }
+    })
+
+
+const q2 = document.getElementsByName("q2");
+console.log(q2);
+q2.forEach((e) => {
+    console.log(e);
+    //e.parentElement.style.backgroundColor = (e.value == "true")? 'green':'red';
+    e.parentElement.style.backgroundColor = 'transparent';
+
+    if (e.checked == true) {
+        if (e.value == "true") {
+            e.parentElement.style.backgroundColor = 'green';
+            rightanswers++;
+        }
+        else {
+            e.parentElement.style.backgroundColor = 'red';
+        }
+    }
+})
+
+const q3 = document.getElementsByName("q3");
+console.log(q3);
+q3.forEach((e) => {
+    console.log(e);
+    //e.parentElement.style.backgroundColor = (e.value == "true")? 'green':'red';
+    e.parentElement.style.backgroundColor = 'transparent';
+
+    if (e.checked == true) {
+        if (e.value == "true") {
+            e.parentElement.style.backgroundColor = 'green';
+            rightanswers++;
+
+        }
+        else {
+            e.parentElement.style.backgroundColor = 'red';
+        }
+    }
+})
+answerElement.innerHTML ="Du Hast "+rightanswers + " Fragen richtig beantwortet"; 
+rightanswers =0;
+
+});
